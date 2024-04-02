c83f1a73951342d48d7ffec1992c457b3040c56d
shlfung <shlfung@icloud.com>
2024-04-02 14:01:18 -0700

    Working example with 2 scenario together

--- .features-gen/features/sample.feature.spec.js
+++ .features-gen/features/sample.feature.spec.js
@@ -1,4 +1,4 @@
-/** Generated from: features\sample.feature */
+/** Generated from: features/sample.feature */
 import { test } from "playwright-bdd";
 
 test.describe("OpenSpecimen Homepage", () => {
@@ -9,6 +9,13 @@ test.describe("OpenSpecimen Homepage", () => {
     await Then("I see in title \"Home\"", null, { page });
   });
 
+  test("Check Side Bar", async ({ Given, page, When, Then }) => {
+    await Given("I open url \"https://openspecimen.bcchr.ca/openspecimen\"", null, { page });
+    await When("I fill in the username and password and click button \"Sign In\"", null, { page });
+    await When("I click on the side bar", null, { page });
+    await Then("I see in title \"Navigate To\"", null, { page });
+  });
+
 });
 
 // == technical section ==
@@ -16,9 +23,10 @@ test.describe("OpenSpecimen Homepage", () => {
 test.use({
   $test: ({}, use) => use(test),
   $testMetaMap: ({}, use) => use(testMetaMap),
-  $uri: ({}, use) => use("features\\sample.feature"),
+  $uri: ({}, use) => use("features/sample.feature"),
 });
 
 const testMetaMap = {
   "Check title": {"pickleLocation":"3:5"},
+  "Check Side Bar": {"pickleLocation":"8:5"},
 };
--- features/sample.feature
+++ features/sample.feature
@@ -4,3 +4,9 @@ Feature: OpenSpecimen Homepage
         Given I open url "https://openspecimen.bcchr.ca/openspecimen"
         When I fill in the username and password and click button "Sign In"
         Then I see in title "Home"
+
+    Scenario: Check Side Bar
+        Given I open url "https://openspecimen.bcchr.ca/openspecimen"
+        When I fill in the username and password and click button "Sign In"
+        When I click on the side bar
+        Then I see in title "Navigate To"
